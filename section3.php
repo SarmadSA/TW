012d03ee64b22547cf172fa11b25f3e08d3357a9
Sarmad <sarmad@techwizzle.com>
2018-02-25 22:31:27 +0100

    Changd HTML code in section 3 to PHP code

--- section3.php
+++ section3.php
@@ -1,10 +1,16 @@
-<article class="content article-second"> 
-	<!--Tittelen øverst på siden (seksjon tittelen)-->	
-	<div class="element-tittle-container">
-		<h3 class="section-tittle">GTX 1060 vs GTX 1070</h3>
-	</div>
-	<!--Bilde beholder-->
-	<figure class="image-container">
-		<img src="images/trondheim.png" alt="trondheim"/>
-	</figure> 				
-</article>
+<?php
+	$numberOfElements = 4;
+	$imageArray = ["trondheim.png","trondheim.png","trondheim.png","trondheim.png"];
+	$directory = "images/";
+	$tittles = ["GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070"];
+	for($i = 0; $i < $numberOfElements; $i++){
+		echo "<article class=\"content article-second\">";
+		echo "<div class=\"element-tittle-container\">";
+		echo "<h3 class=\"section-tittle\">" . $tittles[$i] . "</h3>";
+		echo "</div>";
+		echo "<figure class=\"image-container\">";
+		echo "<img src=\"" . $directory . $imageArray[$i] . "\" alt=\"trondheim\"/>";
+		echo "</figure>";
+		echo "</article>";
+	}
+?>

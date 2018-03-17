125a679557b679aee2a2a87210b03a95116b31fc
Sarmad <sarmad@techwizzle.com>
2018-03-17 10:29:11 +0100

    Refactoring part 3.

--- section3.php
+++ section3.php
@@ -4,13 +4,13 @@
 	$directory = "images/";
 	$tittles = ["GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070"];
 	for($i = 0; $i < $numberOfElements; $i++){
-		echo "<article class=\"content article-second\">";
-		echo "<div class=\"element-tittle-container\">";
-		echo "<h3 class=\"section-tittle\">" . $tittles[$i] . "</h3>";
-		echo "</div>";
-		echo "<figure class=\"image-container\">";
-		echo "<img src=\"" . $directory . $imageArray[$i] . "\" alt=\"trondheim\"/>";
-		echo "</figure>";
-		echo "</article>";
+		echo '<article class="content article-second">
+				<div class="element-tittle-container">
+					<h3 class="section-tittle">' . $tittles[$i] . '</h3>
+				</div>
+				<figure class="image-container">
+					<img src="' . $directory . $imageArray[$i] . '" alt="trondheim"/>
+				</figure>
+			</article>';
 	}
 ?>

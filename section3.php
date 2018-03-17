9f2e60df8231bb815c32bc13bdbff21e51f7d47d
Sarmad <sarmad@techwizzle.com>
2018-03-17 11:49:32 +0100

    Improved code.

--- section3.php
+++ section3.php
@@ -4,13 +4,15 @@
 	$directory = "images/";
 	$tittles = ["GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070","GTX 1060 vs GTX 1070"];
 	for($i = 0; $i < $numberOfElements; $i++){
-		echo '<article class="content article-second">
-				<div class="element-tittle-container">
-					<h3 class="section-tittle">' . $tittles[$i] . '</h3>
-				</div>
-				<figure class="image-container">
-					<img src="' . $directory . $imageArray[$i] . '" alt="trondheim"/>
-				</figure>
-			</article>';
+?>
+		<article class="content article-second">
+			<div class="element-tittle-container">
+				<h3 class="section-tittle"><?php echo $tittles[$i] ?></h3>
+			</div>
+			<figure class="image-container">
+				<img src="<?php echo $directory . $imageArray[$i] ?>" alt="trsondheim"/>
+			</figure>
+		</article>
+<?php
 	}
 ?>

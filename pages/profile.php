6bfc8195958ffaafb87a7d12df3db851a48b494e
Sarmad <sarmad@techwizzle.com>
2018-06-05 17:51:26 +0200

    Tweak coee

---
+++ pages/profile.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Product name</title>
+	<?php include '../templates/styleSheets.php';?>
+</head>
+
+<body>
+	
+	<?php include '../templates/header.php';?>
+	<?php include '../templates/navigationMenu.php';?>
+	<main id="container">
+		<div class="page-tittle">
+			<h2>Profiles</h2>
+		</div>
+		
+		<article class="profile-container">
+			<div class="profile-header">
+				<!--here goes the profile image and other important facts about the product-->
+			</div>
+			<div class="profile-body">
+				<!--here goes all informatioin, testing and testing results about the produck-->
+			</div>
+		</article>
+	
+		<br class="clear"/>
+	</main>
+	<?php include '../templates/footer.php';?>
+</body>
+</html>
---
+++ templates/newPage.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Techizzle template page</title>
+	<?php include '../templates/styleSheets.php';?>
+</head>
+
+<body>
+	
+	<?php include '../templates/header.php';?>
+	<?php include '../templates/navigationMenu.php';?>
+	<main id="container">
+		<div class="page-tittle">
+			<h2>Page tittle</h2>
+		</div>	
+	
+		<br class="clear"/>
+	</main>
+	<?php include '../templates/footer.php';?>
+</body>
+</html>

14be944c05e28d8741388134b149df0081325b6c
Sarmad <sarmad@techwizzle.com>
2018-06-06 17:12:59 +0200

    Refactor nav menu

--- templates/navigationMenu.php
+++ templates/navigationMenu.php
@@ -1,35 +1,17 @@
-<nav id="navigation-menu-container">
-	<ul id="navigationUL">
-		<!--Navigationlist-->
-		<li class="navigation-list"><a href="../index.php">Hjem</a></li>
-		<li class="dropdown navigation-list">
-			<a href="artikler.html">Artikler</a>
-			<div class="dropdown-content" id="myDropdown">
-				<a href="vitenskapeligArtikler.html">Vitenskapelig artikler</a>
-				<a href="andreArtikler.html">Andre artikler</a>
-			</div>
-		</li>
-		<li class="navigation-list"><a href="om.html">Om nettsiden</a></li> 
-		<li class="navigation-list"><a href="../techWizzle/pages/contact.php">Contact us</a></li> 
-		<!--End of navigationlist-->
-
-		<!--Searchfield and searchbutton-->
-		<li>
-			<form class="search-form">
-				<input type="text" class="search" id="search" placeholder="Søk...">
-				<button type="button" id="search_button">
-					<svg viewBox="0 0 36.02 40.02">
-					<defs>
-					<symbol id="search_symbol" viewBox="0 0 32.87 36.83">
-						<circle class="cls-1" cx="12.5" cy="12.5" r="10"/>
-						<line class="cls-1" x1="20.53" y1="19.50" x2="32.5" y2="29.5"/>
-					</symbol>
-					</defs>
-					<use width="20" height="20" transform="translate(6.5 8.5) scale(1.2 1.2)" xlink:href="#search_symbol"/>
-					</svg>	
-				</button>
-			</form>
-		</li>
-		<!--End of av searchfield and searchbutton-->
+<nav id="nav-menu" class="nav-menu">
+    <!--Logo container-->
+    <a href="index.php#home-section">
+        <div class="logo-container">
+            <!--SVG logo-->
+            <?php include'techwizzle-logo.svg';?>
+            <!--Logo text-->
+            <h1 class="logo-text">TechWizzle</h1>
+        </div>
+    </a>   
+	<ul id="navigation-ul">
+		<li class="list-element"><a href="#">Profiles</a></li>
+		<li class="list-element"><a href="#">Compare</a></li>
+		<li class="list-element"><a href="#">About</a></li> 
+		<li class="list-element"><a href="../techWizzle/pages/contact.php">Contact us</a></li> 
 	</ul>
-</nav>	
+</nav>

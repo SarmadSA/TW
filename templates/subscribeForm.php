166e0b0fbf7f99023416ab892c8f799ed926fddb
Sarmad <sarmad@techwizzle.com>
2018-06-06 17:06:53 +0200

    Add subscribe form

---
+++ templates/subscribeForm.php
@@ -0,0 +1,9 @@
+<?php
+    //validation of input
+
+?>
+<form action="index.php" method="post">
+    <P>Enter your email address:</P>
+    <input class="email-input focus-style" type="email" placeholder="Email..">
+    <input class="submit-btn" type="submit" value="Subscribe">
+</form>

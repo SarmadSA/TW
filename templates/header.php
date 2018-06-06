eabf12506e03fbf695d2dc2f1128033da533ad66
Sarmad <sarmad@techwizzle.com>
2018-06-06 17:12:31 +0200

    Update header

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,6 @@
 <header>
-	<h1 class="tech-header">Tech</h1><h1 class="wizzle-header">Wizzle</h1>
+	<div class="logo-box">
+		<?php include'techwizzle-logo.svg';?>
+	</div>
+	<h1 class="tech-header">TechWizzle</h1><h1 class="wizzle-header">Wizzle</h1>
 </header>

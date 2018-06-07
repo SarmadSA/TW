83e6a1865785575591edfe4deb5471dc28e743f7
Sarmad <sarmad@techwizzle.com>
2018-06-07 11:39:13 +0200

    Create profilepage template

---
+++ templates/profilepage.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="nb">
+<head>
+	<link rel="shortcut icon" href="images/techwizzle-icon.png">
+	<meta charset="utf-8">
+	<title>Profile name</title>
+	<link rel="stylesheet" type="text/css" href="../styles/mainStyle.css">
+	<link rel="stylesheet" type="text/css" href="../styles/footer.css">
+	<link rel="stylesheet" type="text/css" href="../styles/logo.css">
+	<link rel="stylesheet" type="text/css" href="../styles/form.css">
+	<link rel="stylesheet" type="text/css" href="../styles/header.css">
+	<link rel="stylesheet" type="text/css" href="../styles/sliderStyle.css">
+	<link rel="stylesheet" type="text/css" href="../styles/navigationMenueStyle.css">
+	<link rel="stylesheet" type="text/css" href="../styles/searchBarStyle.css">
+	<link rel="stylesheet" type="text/css" href="../styles/contentStyle.css">
+	<link rel="stylesheet" type="text/css" href="../styles/productProfileStyle.css">
+	<link rel="stylesheet" type="text/css" href="../styles/profilepage.css">
+	<link href="https://fonts.googleapis.com/css?family=Abel|Saira+Condensed" rel="stylesheet">
+</head>
+<body>
+	<header><?php include 'navigationMenu.php';?></header>
+	<main class="main">
+
+        <section class="profile-section">
+
+        </section>
+
+		<aside>
+			<h2>Get the latest updates</h2>
+			<br>
+			<?php include 'subscribeForm.php';?>  
+		</aside>
+		
+		<br class="clear"/>
+	</main>
+	
+	<!--Slutten av "container"-->	
+	<?php include 'footer.php';?>
+	
+</body>
+</html>
